8586a228a2697d2798614309b009214ef984c15b
daehyeon112 <146954798+daehyeon112@users.noreply.github.com>
2024-01-24 22:21:12 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const age =parseInt( prompt("How old are you?"));
-if (isNaN(age)) {
-    alert("please write a numebr!");
-} else {
-    alert("thank you for writing your age!");
+const age = parseInt(prompt("how old are you?"));
+
+if(isNaN(age) || age < 0 ){
+    alert("please write a real positive number");
+} else if(age < 18){
+    alert("you are too young");
+} else if(age >= 18 && age <= 50){
+    alert("you can drink");
+} else if(age > 50 && age <= 80){
+    alert("you should exercise");
+} else if(age > 80){
+    alert("you can do whatever you want");
 }
+
